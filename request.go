e09ffbb2cb9ec82effc942ffade89119f00e384a
vvakame <vvakame+dev@gmail.com>
2017-11-13 02:06:47 +0900

    feat(ucon): change signature interface{} to HandlerContainer about Bubble#RequestHandler

--- request.go
+++ request.go
@@ -24,7 +24,7 @@ type Bubble struct {
 	R              *http.Request
 	W              http.ResponseWriter
 	Context        context.Context
-	RequestHandler interface{}
+	RequestHandler HandlerContainer
 
 	Debug bool
 
